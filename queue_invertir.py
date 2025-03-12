e5a44c0d67764078f872dc061c0f20f18541e1e0
David <hruales1138@gmail.com>
2025-03-12 09:50:45 -0500

    commit

---
+++ queue_invertir.py
@@ -0,0 +1,26 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+        
+    def enqueue(self, n):
+        self.queue.append(n)
+    
+    def dequeue(self):
+        return self.queue.pop(0)
+    
+    def fist(self):
+        return self.queue[0]
+    
+    
+q = Queue()
+print(q.queue)
+q.enqueue(3)
+print(q.queue)
+q.enqueue(4)
+print(q.queue)
+q.enqueue(5)
+print(q.queue)
+q.dequeue()
+print(q.queue)
+q.dequeue()
+print(q.queue)
